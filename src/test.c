160392cb77d487a8742300b2f0a223f24a3d33bd
Yasu Watanabe <ywata@protonmail.com>
2021-07-23 07:05:45 +0900

    Which function is defined on mingw64.

--- src/test.c
+++ src/test.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
+  int fd = 1;
+  const char *mode = "r";
 #ifdef _WIN32
 #warning "_WIN32 is defined"
 #else /* _WIN32 */
 #warning "_WIN32 is not defined"
 #endif
+  FILE *f1 = _fdopen(fd, mode);
+  FILE *f2 = fdopen(fd, mode);
+  
   return 0;
 }
